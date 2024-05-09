f983fa2d8d102722dcac059c1b3d7c2c9e2801e5
202210119 <202210119@feualabang.edu.ph>
2024-05-09 22:55:57 +0800

    Update teacher_homepage.py

--- teacher_homepage.py
+++ teacher_homepage.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from authentication import logout
 
-def teacher_homepage(username):
-    st.title(f"Welcome, Teacher {username}!")
+def teacher_homepage():
+    st.title("Teacher Homepage")
     st.write("This is the Teacher homepage.")
     if st.button("Logout"):
         logout()
