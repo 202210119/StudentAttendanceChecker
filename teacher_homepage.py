6f7fb3cf3fa858fef69b95eb8f7ad22bc6d80756
202210119 <202210119@feualabang.edu.ph>
2024-05-09 23:05:57 +0800

    Update teacher_homepage.py

--- teacher_homepage.py
+++ teacher_homepage.py
@@ -4,6 +4,3 @@ from authentication import logout
 def teacher_homepage():
     st.title(f"Welcome, Teacher {st.session_state.username}!")
     st.write("This is the Teacher homepage.")
-    if st.button("Logout"):
-        logout()
-        st.experimental_rerun()  # Rerun the app to show the login page
