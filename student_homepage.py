8e9d45cf9bc808d1254787f514e431ec3133ed60
202210119 <202210119@feualabang.edu.ph>
2024-05-09 23:07:51 +0800

    Update student_homepage.py

--- student_homepage.py
+++ student_homepage.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from authentication import logout
+from streamlit_delta_calendar import DeltaCalendar
 
 def student_homepage():
     st.title(f"Welcome, Student {st.session_state.username}!")
     st.write("This is the Student homepage.")
+
+    # Add a calendar for students
+    st.write("Student Calendar:")
+    calendar = DeltaCalendar()
+    calendar
