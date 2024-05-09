28877f068d049e5a16bc3d21cb61bacca7ef35bf
202210119 <202210119@feualabang.edu.ph>
2024-05-09 23:05:35 +0800

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,11 +1,17 @@
 import streamlit as st
-from authentication import login
+from authentication import login, logout
 
 def login_page():
     st.title("Login")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
-    if st.button("Login"):
-        if login(username, password):
-            st.experimental_rerun()  # Rerun the app to show the homepage
-
+    
+    # Check if user is logged in to show logout button
+    if st.session_state.get("logged_in", False):
+        if st.button("Logout"):
+            logout()
+            st.experimental_rerun()  # Rerun the app to show the login page
+    else:
+        if st.button("Login"):
+            if login(username, password):
+                st.experimental_rerun()  # Rerun the app to show the homepage
