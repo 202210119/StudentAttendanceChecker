c0598f2d0ec0182db2c3bed45dae17826d6c86c0
202210119 <202210119@feualabang.edu.ph>
2024-05-09 23:02:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,8 @@ def main():
     # Display Teacher or Student homepage if logged in
     if st.session_state.get("logged_in", False):
         if st.session_state.user_type == "teacher":
-            st.title(f"Welcome, Teacher {st.session_state.username}!")
             teacher_homepage()
         elif st.session_state.user_type == "student":
-            st.title(f"Welcome, Student {st.session_state.username}!")
             student_homepage()
 
 if __name__ == "__main__":
