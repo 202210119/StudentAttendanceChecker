98e041c6d60380552be69f01a27f1a462f5fc31a
202210119 <202210119@feualabang.edu.ph>
2024-05-09 23:11:06 +0800

    Update student_homepage.py

--- student_homepage.py
+++ student_homepage.py
@@ -2,11 +2,11 @@ import streamlit as st
 from authentication import logout
 from streamlit_calendar import st_calendar
 
-def teacher_homepage():
-    st.title(f"Welcome, Teacher {st.session_state.username}!")
-    st.write("This is the Teacher homepage.")
+def student_homepage():
+    st.title(f"Welcome, Student {st.session_state.username}!")
+    st.write("This is the Student homepage.")
 
-    # Add a calendar for teachers
-    st.write("Teacher Calendar:")
-    date_value = st_calendar(label="Select a date", key="teacher_calendar")
+    # Add a calendar for students
+    st.write("Student Calendar:")
+    date_value = st_calendar(label="Select a date", key="student_calendar")
     st.write(f"Selected date: {date_value}")
