063e2ad066cb8fed7e7643429e2ae4b8dcb49390
202210119 <202210119@feualabang.edu.ph>
2024-05-10 20:47:54 +0800

    Update student.py

--- student.py
+++ student.py
@@ -12,12 +12,21 @@ class Student:
             st.session_state.classes = {}
 
     def join_class(self, class_name):
+        # Check if the class_name exists in session state
         if class_name in st.session_state.classes:
+            # Check if the student is already in the class
             if st.session_state.username not in st.session_state.classes[class_name]:
-                if class_name not in st.session_state.classes:
-                    st.session_state.classes[class_name] = {}
-                st.session_state.classes[class_name][st.session_state.username] = None
-        return False
+                # Add the student to the class
+                st.session_state.classes[class_name].append(st.session_state.username)
+                return True  # Return True on successful join
+            else:
+                st.warning("You are already in this class.")
+        else:
+            # If the class doesn't exist, create a new entry and add the student
+            st.session_state.classes[class_name] = [st.session_state.username]
+            return True  # Return True on successful join
+
+        return False  # Return False if joining fails
 
     def get_student_classes(self):
         return list(st.session_state.classes.keys())
