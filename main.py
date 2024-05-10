3ba402f4cc60afd205df95ccdc8b40f9d5d4cc06
202210119 <202210119@feualabang.edu.ph>
2024-05-10 21:26:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,13 +31,14 @@ def main():
         login_page()
     elif page == "Register":
         register_page()
-    elif page in ["Teacher Homepage", "Student Homepage"]:
-        homepage(st.session_state.username, st.session_state.user_type)
     elif page == "Class Page":
-        class_page(st.session_state.username, st.session_state.selected_class)
-    else:
-        if page in teacher_instance.get_teacher_classes():
+        if st.session_state.user_type == "teacher":
+            class_page(st.session_state.username, st.session_state.selected_class)
+        else:
             class_page(st.session_state.username, page)
+    else:
+        homepage(st.session_state.username, st.session_state.user_type)
+
 
 if __name__ == "__main__":
     main()
