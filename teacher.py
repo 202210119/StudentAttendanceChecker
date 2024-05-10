266c6dad6c778244f3ef611f7ffdd6e8a73b383b
202210119 <202210119@feualabang.edu.ph>
2024-05-10 17:54:53 +0800

    Update teacher.py

--- teacher.py
+++ teacher.py
@@ -1,17 +1,26 @@
+# teacher.py
+import streamlit as st
+
 class Teacher:
-    def __init__(self, username):
-        self.username = username
-        self.classes = {}
+    @staticmethod
+    def get_teacher():
+        if "teacher" not in st.session_state:
+            st.session_state.teacher = Teacher()
+        return st.session_state.teacher
+
+    def __init__(self):
+        if "classes" not in st.session_state:
+            st.session_state.classes = {}
 
     def create_class(self, class_name):
-        if class_name not in self.classes:
-            self.classes[class_name] = {}
+        if class_name not in st.session_state.classes:
+            st.session_state.classes[class_name] = {}
             return True
         else:
             return False
 
     def get_teacher_classes(self):
-        return list(self.classes.keys())
+        return list(st.session_state.classes.keys())
 
     def get_class_schedule(self, class_name):
-        return self.classes.get(class_name, {})
+        return st.session_state.classes.get(class_name, {})
