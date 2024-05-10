eff4f62a6fe96590d656b53a80bd12eb2326d74a
202210119 <202210119@feualabang.edu.ph>
2024-05-10 19:06:17 +0800

    Update student_homepage.py

--- student_homepage.py
+++ student_homepage.py
@@ -1,6 +1,8 @@
+# student_homepage.py
 import streamlit as st
 from authentication import logout
 from student import Student
+from teacher import Teacher
 
 def student_homepage(username):
     st.title(f"Welcome, Student {username}!")
@@ -9,15 +11,13 @@ def student_homepage(username):
     class_code = st.text_input("Enter Class Code:")
     if st.button("Join"):
         student = Student.get_student(username)
-        if student is not None:
-            if student.join_class(class_code):
-                st.success(f"You have joined the class with code '{class_code}'.")
-            else:
-                st.error("Invalid class code.")
+        teacher_instance = Teacher.get_teacher()
+        if student.join_class(class_code, teacher_instance.get_teacher_classes()):
+            st.success(f"You have joined the class with code '{class_code}'.")
 
-    student = Student.get_student(username)
-    if student is not None:
-        student_classes = student.get_student_classes()
+    student_instance = Student.get_student(username)
+    if student_instance is not None:  # Check if student_instance is not None
+        student_classes = student_instance.get_student_classes()
         st.header("Your Classes")
         if student_classes:
             for class_name in student_classes:
