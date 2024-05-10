49e19a6c66820c9f59da25a2d75578dcbec06426
202210119 <202210119@feualabang.edu.ph>
2024-05-10 19:31:10 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,27 +1,26 @@
-# student.py
 import streamlit as st
 from teacher import Teacher
 
 class Student:
-    @staticmethod
-    def get_student(username):
-        if "student" not in st.session_state:
-            st.session_state.student = Student()
-        return st.session_state.student
+  @staticmethod
+  def get_student(username):
+    if "student" not in st.session_state:
+      st.session_state.student = Student()
+    return st.session_state.student
 
-    def __init__(self):
-        if "classes" not in st.session_state:
-            st.session_state.classes = {}
+  def __init__(self):
+    if "classes" not in st.session_state:
+      st.session_state.classes = []  # Initialize as an empty list
 
-    def join_class(self, class_code, teacher_classes):
-        if class_code in teacher_classes:
-            if class_code not in st.session_state.classes:
-                st.session_state.classes[class_code] = [st.session_state.username]  # Initialize the class code key with a list containing the username
-            else:
-                st.session_state.classes[class_code].append(st.session_state.username)  # Append the username to the list
-            return True
-        else:
-            return False
+  def join_class(self, class_code, teacher_classes):
+    if class_code in teacher_classes:
+      if class_code not in st.session_state.classes:
+        st.session_state.classes.append([st.session_state.username])  # Create a new list with username
+      else:
+        st.session_state.classes.extend([st.session_state.username])  # Add username to existing list
+      return True
+    else:
+      return False
 
-    def get_student_classes(self):
-        return list(st.session_state.classes.keys())
+  def get_student_classes(self):
+    return list(st.session_state.classes.keys())
