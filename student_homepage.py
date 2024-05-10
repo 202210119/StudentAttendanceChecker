834cd803266a12c6ebb07a649d124387c883c1a0
202210119 <202210119@feualabang.edu.ph>
2024-05-10 18:50:35 +0800

    Update student_homepage.py

--- student_homepage.py
+++ student_homepage.py
@@ -1,4 +1,3 @@
-# student_homepage.py
 import streamlit as st
 from authentication import logout
 from student import Student
@@ -10,20 +9,9 @@ def student_homepage(username):
     st.header("Join a Class")
     class_code = st.text_input("Enter Class Code:")
     if st.button("Join"):
-        student = Student.get_student(username)
-        teacher_instance = Teacher.get_teacher()
-        if student.join_class(class_code, teacher_instance.get_teacher_classes()):
+        student_instance = Student.get_student(username)
+        if student_instance.join_class(class_code, class_code, Teacher.get_teacher().get_teacher_classes()):
             st.success(f"You have joined the class with code '{class_code}'.")
-
-    student_instance = Student.get_student(username)
-    student_classes = student_instance.get_student_classes()
-
-    st.header("Your Classes")
-    if student_classes:
-        for class_name in student_classes:
-            st.write(class_name)
-    else:
-        st.write("You haven't joined any classes yet.")
 
     if st.button("Logout"):
         logout()
