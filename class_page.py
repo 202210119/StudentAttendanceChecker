1697e950ae7727077b2e5352e29361065f077494
202210119 <202210119@feualabang.edu.ph>
2024-05-10 21:52:55 +0800

    Update class_page.py

--- class_page.py
+++ class_page.py
@@ -1,12 +1,9 @@
 # class_page.py
-
 import streamlit as st
 from authentication import logout
 from teacher import Teacher
-
 def class_page(username, class_name):
     st.title(f"Class: {class_name}")
-
     # Display option to create a new event
     st.header("Add Event to Schedule")
     event_time = st.text_input("Enter Event Time:")
@@ -15,17 +12,16 @@ def class_page(username, class_name):
         teacher = Teacher.get_teacher()
         if teacher.add_event_to_schedule(class_name, event_time, event_description):
             st.success("Event added successfully.")
-
     # Display class schedule
     st.header("Class Schedule")
     teacher = Teacher.get_teacher()
     class_schedule = teacher.get_class_schedule(class_name)
+    st.write("Class Schedule data type:", type(class_schedule))  # Print data type
     if class_schedule:
         for event_time, event_description in class_schedule.items():
             st.write(f"- {event_time}: {event_description}")
     else:
         st.info("No events in the schedule yet.")
-
     # Option to delete the class
     if st.button("Delete Class"):
         teacher = Teacher.get_teacher()
