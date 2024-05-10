9433f6af4164262f831d2d7512712bd5e6febaa2
202210119 <202210119@feualabang.edu.ph>
2024-05-10 18:55:51 +0800

    Update student_homepage.py

--- student_homepage.py
+++ student_homepage.py
@@ -10,20 +10,20 @@ def student_homepage(username):
     st.header("Join a Class")
     class_code = st.text_input("Enter Class Code:")
     if st.button("Join"):
-        student_instance = Student.get_student(username)
+        student = Student.get_student(username)
         teacher_instance = Teacher.get_teacher()
-        if student_instance.join_class(class_code, class_code, teacher_instance.get_teacher_classes()):
+        if student.join_class(class_code, teacher_instance.get_teacher_classes()):
             st.success(f"You have joined the class with code '{class_code}'.")
 
     student_instance = Student.get_student(username)
-    student_classes = student_instance.get_student_classes()
-
-    st.header("Your Classes")
-    if student_classes:
-        for class_name in student_classes:
-            st.write(class_name)
-    else:
-        st.write("You haven't joined any classes yet.")
+    if student_instance is not None:  # Check if student_instance is not None
+        student_classes = student_instance.get_student_classes()
+        st.header("Your Classes")
+        if student_classes:
+            for class_name in student_classes:
+                st.write(class_name)
+        else:
+            st.write("You haven't joined any classes yet.")
 
     if st.button("Logout"):
         logout()
