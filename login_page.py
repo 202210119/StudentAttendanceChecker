4be280878dce216f057003e77b9c8c47cdf484cd
202210119 <202210119@feualabang.edu.ph>
2024-05-10 16:38:12 +0800

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -6,12 +6,11 @@ def login_page():
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     
-    # Check if user is logged in to show logout button
     if st.session_state.get("logged_in", False):
         if st.button("Logout"):
             logout()
-            st.experimental_rerun()  # Rerun the app to show the login page
+            st.ererun()
     else:
         if st.button("Login"):
             if login(username, password):
-                st.experimental_rerun()  # Rerun the app to show the homepage
+                st.rerun()
