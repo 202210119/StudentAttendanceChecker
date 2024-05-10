8986a4b34da35bd52c2b5e080464aec0ae4b9e2a
202210119 <202210119@feualabang.edu.ph>
2024-05-10 19:34:34 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,7 +1,7 @@
 # homepage.py
 import streamlit as st
-from teacher import Teacher
 from student import Student
+from teacher import Teacher
 
 def homepage(username, user_type):
     st.title(f"Welcome, {user_type.capitalize()} {username}!")
@@ -32,24 +32,14 @@ def teacher_homepage(username):
         st.info("You haven't created any classes yet.")
 
 def student_homepage(username):
-    st.header("Join a Class")
-    class_code = st.text_input("Enter Class Code:")
-    if st.button("Join"):
-        student = Student.get_student(username)
-        teacher_instance = Teacher.get_teacher()
-        if student.join_class(class_code, teacher_instance.get_teacher_classes()):
-            st.success(f"You have joined the class with code '{class_code}'.")
-            st.session_state.selected_class = class_code
-            st.experimental_rerun()  # Reload the app to go to the selected class
+    student = Student.get_student(username)
+    teacher_instance = Teacher.get_teacher()
+    student.join_classes(teacher_instance.get_teacher_classes())
 
+    student_classes = student.get_student_classes()
     st.header("Your Classes")
-    student_instance = Student.get_student(username)
-    student_classes = student_instance.get_student_classes()
     if student_classes:
-        selected_class = st.selectbox("Select Class", [""] + student_classes)
-        if selected_class:
-            if st.button("Go to Class"):
-                st.session_state.selected_class = selected_class
-                st.experimental_rerun()  # Reload the app to go to the selected class
+        for class_name in student_classes:
+            st.write(class_name)
     else:
-        st.info("You haven't joined any classes yet.")
+        st.write("You haven't joined any classes yet.")
