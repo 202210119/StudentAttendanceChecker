00be5bbdb2bdb707dc55c2940067b4d3b08d09da
202210119 <202210119@feualabang.edu.ph>
2024-05-10 22:21:06 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,28 +1,21 @@
-# homepage.py
-
 import streamlit as st
 from authentication import logout
 from teacher import Teacher
 from student import Student
-
 def homepage(username, user_type):
     st.title(f"Welcome, {user_type.capitalize()} {username}!")
-
     if user_type == "teacher":
         create_teacher_class(username)
     elif user_type == "student":
         join_class(username)
-
 def create_teacher_class(username):
     st.title(f"Welcome, Teacher {username}!")
-
     st.header("Create a New Class")
     class_name = st.text_input("Enter Class Name:")
     if st.button("Create Class"):
         teacher = Teacher.get_teacher()  # Get the teacher instance
         if teacher.create_class(class_name):
             st.success(f"Class '{class_name}' created successfully.")
-
     # Display existing classes
     st.header("Your Classes")
     teacher = Teacher.get_teacher()  # Get the teacher instance
@@ -35,7 +28,6 @@ def create_teacher_class(username):
                 st.experimental_rerun()
     else:
         st.info("You haven't created any classes yet.")
-
 def join_class(username):
     st.header("Join a Class")
     st.info("Select a class to join:")
@@ -50,10 +42,8 @@ def join_class(username):
                     st.session_state.selected_class = selected_class
                 else:
                     st.error(f"Failed to join the class '{selected_class}'.")
-
     else:
         st.info("No classes available to join.")
-
     if st.button("Logout"):
         logout()
         st.experimental_rerun()
