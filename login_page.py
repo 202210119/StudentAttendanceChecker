cbd012122d908e1e7dcbee9107caa29e7ac58d31
202210119 <202210119@feualabang.edu.ph>
2024-05-10 18:38:14 +0800

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -2,17 +2,16 @@ import streamlit as st
 from authentication import login, logout
 
 def login_page():
-    st.title("Attendance Checker")    
-
     st.title("Login")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     
     if st.session_state.get("logged_in", False):
+        st.info(f"You are logged in as {st.session_state.user_type.capitalize()}")
         if st.button("Logout"):
             logout()
-            st.rerun()
+            st.experimental_rerun()
     else:
         if st.button("Login"):
             if login(username, password):
-                st.rerun()
+                st.experimental_rerun()
