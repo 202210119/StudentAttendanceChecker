c0a66c53883ccb716e37de798bd71824a0dc1c9b
202210119 <202210119@feualabang.edu.ph>
2024-05-10 21:09:29 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,5 +1,3 @@
-import streamlit as st
-
 class Student:
     @staticmethod
     def get_student():
@@ -9,24 +7,27 @@ class Student:
 
     def __init__(self):
         if "classes" not in st.session_state:
-            st.session_state.classes = {}
+            st.session_state.classes = {}  # Dictionary to store class information
 
     def join_class(self, class_name):
+        # Check if the class_name exists
         if class_name in st.session_state.classes:
             if st.session_state.username not in st.session_state.classes[class_name]:
-                # If the class exists and the student is not already in it, add the student
-                if not isinstance(st.session_state.classes[class_name], list):
-                    st.session_state.classes[class_name] = [st.session_state.classes[class_name]]  # Initialize as list
                 st.session_state.classes[class_name].append(st.session_state.username)
                 return True  # Return True on successful join
             else:
                 st.warning("You are already in this class.")
         else:
-            # If the class doesn't exist, create it and add the student
             st.session_state.classes[class_name] = [st.session_state.username]
             return True  # Return True on successful join
 
         return False  # Return False if joining fails
 
     def get_student_classes(self):
-        return list(st.session_state.classes.keys())
+        return list(st.session_state.classes.keys())  # Return list of class names
+
+    def get_all_enrolled_students(self):
+        all_students = []
+        for class_name, enrolled_users in st.session_state.classes.items():
+            all_students.extend(enrolled_users)  # Extend the list with all usernames
+        return all_students
