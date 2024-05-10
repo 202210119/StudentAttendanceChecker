66777003b8e9dc5e15ec7a605e2cd739998d95ba
202210119 <202210119@feualabang.edu.ph>
2024-05-10 18:58:42 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,24 +1,28 @@
-# student.py
+import streamlit as st
+from authentication import logout
+from teacher import Teacher  # Import the Teacher class
 
 class Student:
     @staticmethod
     def get_student(username):
+        # Check if "student" key exists in session_state and return None if it doesn't
         if "student" not in st.session_state:
-            st.session_state.student = Student()
+            return None
         return st.session_state.student
 
-    def __init__(self):
-        if "classes" not in st.session_state:
-            st.session_state.classes = {}
+    def __init__(self, username):
+        self.username = username
 
-        self.classes = st.session_state.classes
-
-    def join_class(self, class_name, teacher_classes):
-        if class_name in teacher_classes:
-            self.classes[class_name] = class_name
-            return True 
+    def join_class(self, class_code, teacher_classes):
+        # Check if the class code exists in the teacher's classes
+        if class_code in teacher_classes:
+            # Add the student to the class
+            if class_code not in self.classes:
+                self.classes[class_code] = []
+            self.classes[class_code].append(self.username)
+            return True
         else:
-            return False 
+            return False
 
     def get_student_classes(self):
         return list(self.classes.keys())
