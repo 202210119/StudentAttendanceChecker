75514303afc133d5e51f39bef8c0c9327e1b5df3
202210119 <202210119@feualabang.edu.ph>
2024-05-10 17:06:14 +0800

    Update student_homepage.py

--- student_homepage.py
+++ student_homepage.py
@@ -3,13 +3,8 @@ from authentication import logout
 
 def student_homepage(username):
     st.title(f"Welcome, Student {username}!")
-    st.write("This is the Student homepage.")
-
+    
     st.header("Join a Class")
     class_code = st.text_input("Enter Class Code:")
     if st.button("Join"):
         st.success(f"You have joined the class with code '{class_code}'.")
-
-    if st.button("Logout"):
-        logout()
-        st.rerun() 
