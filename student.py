2a55d9b6b498e1b8310331aaff61813d57316e72
202210119 <202210119@feualabang.edu.ph>
2024-05-10 22:03:12 +0800

    Update student.py

--- student.py
+++ student.py
@@ -26,5 +26,8 @@ class Student:
 
         return False
 
-    def get_student_classes(self):
-        return list(st.session_state.classes.keys())
+     def get_student_classes(self):
+            if st.session_state.classes:
+                return [class_name for class_name, students in st.session_state.classes.items() if st.session_state.username in students]
+            else:
+                return []
