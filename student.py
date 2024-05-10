81648a43acc3f3cf2db9e6c09d0c1c84b65a2887
202210119 <202210119@feualabang.edu.ph>
2024-05-10 21:40:37 +0800

    Update student.py

--- student.py
+++ student.py
@@ -25,5 +25,10 @@ class Student:
 
         return False  # Return False if joining fails
 
-    def get_student_classes(self):
-        return list(st.session_state.classes.keys())
+def get_student_classes(self):
+    if "username" not in st.session_state:
+        return []
+
+    student_username = st.session_state.username
+    return [class_name for class_name, students in st.session_state.classes.items() if student_username in students]
+
