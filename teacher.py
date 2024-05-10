7af9d52c33cceb6e1d426f2b2be559eea0f44fc7
202210119 <202210119@feualabang.edu.ph>
2024-05-10 17:59:22 +0800

    Update teacher.py

--- teacher.py
+++ teacher.py
@@ -13,8 +13,17 @@ class Teacher:
             st.session_state.classes = {}
 
     def create_class(self, class_name):
+        if class_name == "":
+            return False
         if class_name not in st.session_state.classes:
             st.session_state.classes[class_name] = {}
+            return True
+        else:
+            return False
+
+    def delete_class(self, class_name):
+        if class_name in st.session_state.classes:
+            del st.session_state.classes[class_name]
             return True
         else:
             return False
@@ -24,3 +33,10 @@ class Teacher:
 
     def get_class_schedule(self, class_name):
         return st.session_state.classes.get(class_name, {})
+
+    def add_event_to_schedule(self, class_name, event_time, event_description):
+        if class_name in st.session_state.classes:
+            if event_time != "" and event_description != "":
+                st.session_state.classes[class_name][event_time] = event_description
+                return True
+        return False
