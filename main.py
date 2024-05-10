22ace51f8ccd60086f817dde6f2ac2ac65642db2
202210119 <202210119@feualabang.edu.ph>
2024-05-10 18:27:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+# main.py
 import streamlit as st
 from login_page import login_page
 from register_page import register_page
 from teacher_homepage import teacher_homepage
 from student_homepage import student_homepage
 from class_page import class_page  # Import the class page script
-from create_class_page import create_class_page  # Import the create class page script
-from teacher import Teacher  # Import the Teacher class
 from initialize import initialize_session_state
 
 def main():
-    initialize.initialize_session_state()
+    initialize_session_state()
     
     st.title("Simple Login and Register App")
 
@@ -19,8 +18,8 @@ def main():
         if st.session_state.user_type == "teacher":
             pages.extend(["Teacher Homepage"])
             # Add class creation and class selection options dynamically
-            teacher_instance = teacher.Teacher.get_teacher()
-            for class_name in teacher_instance.get_teacher_classes():
+            teacher = Teacher.get_teacher()
+            for class_name in teacher.get_teacher_classes():
                 pages.append(class_name)
             pages.append("Create Class")
         elif st.session_state.user_type == "student":
@@ -33,19 +32,22 @@ def main():
     page = st.sidebar.radio("Go to", pages)
 
     if page == "Login":
-        login_page.login_page()
+        login_page()
     elif page == "Register":
-        register_page.register_page()
+        register_page()
     elif page == "Teacher Homepage":
-        teacher_homepage.teacher_homepage(st.session_state.username)
+        teacher_homepage(st.session_state.username)
     elif page == "Student Homepage":
-        student_homepage.student_homepage(st.session_state.username)
+        student_homepage(st.session_state.username)
+    elif page == "Create Class":
+        # Show the create class page
+        create_class_page()
     elif page == "Class Page":
-        class_page.class_page(st.session_state.username, st.session_state.selected_class)
+        class_page(st.session_state.username, st.session_state.selected_class)
     else:
         # Check if the selected page is a class page
-        if page in teacher_instance.get_teacher_classes():
-            class_page.class_page(st.session_state.username, page)
+        if page in teacher.get_teacher_classes():
+            class_page(st.session_state.username, page)
 
 if __name__ == "__main__":
     main()
