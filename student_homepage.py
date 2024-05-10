34c12d65792ca00da7fa4a12bf9f03fbfb51d49b
202210119 <202210119@feualabang.edu.ph>
2024-05-10 17:31:02 +0800

    Update student_homepage.py

--- student_homepage.py
+++ student_homepage.py
@@ -1,8 +1,7 @@
-# student_homepage.py
 import streamlit as st
 from authentication import logout
 import pandas as pd
-import datetime
+import requests
 
 def student_homepage(username):
     st.title(f"Welcome, Student {username}!")
@@ -16,6 +15,8 @@ def student_homepage(username):
     schedule_placeholder.table(schedule_df)
 
     while True:
-        current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
+        current_time = requests.get("http://worldtimeapi.org/api/timezone/Etc/UTC").json()["datetime"]
+        current_time = pd.to_datetime(current_time)
+        current_time = current_time.strftime("%I:%M:%S %p")
         current_time_placeholder.write(f"## Current Time: {current_time}")
 
