173588ab88c44e455a148b9eaf0caeee36f9294e
202210119 <202210119@feualabang.edu.ph>
2024-05-10 17:11:42 +0800

    Update teacher_homepage.py

--- teacher_homepage.py
+++ teacher_homepage.py
@@ -1,9 +1,14 @@
 import streamlit as st
 from authentication import logout
 from teacher import Teacher
+import datetime
 
 def teacher_homepage(username):
     st.title(f"Welcome, Teacher {username}!")
+
+    st.write("# Current Time:")
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    st.write(f"## {current_time}")
 
     st.header("Create a Class")
     class_name = st.text_input("Enter Class Name:")
