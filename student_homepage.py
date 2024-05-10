0ddd42ff0aed0d711d87e9a398023305047427ff
202210119 <202210119@feualabang.edu.ph>
2024-05-10 17:15:45 +0800

    Update student_homepage.py

--- student_homepage.py
+++ student_homepage.py
@@ -1,15 +1,20 @@
 import streamlit as st
 from authentication import logout
 import datetime
+import time
 
 def student_homepage(username):
     st.title(f"Welcome, Student {username}!")
 
-    st.write("# Current Time:")
-    current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
-    st.write(f"## {current_time}")
+    current_time_placeholder = st.empty()
 
     st.header("Join a Class")
     class_code = st.text_input("Enter Class Code:")
     if st.button("Join"):
         st.success(f"You have joined the class with code '{class_code}'.")
+
+    while True:
+        current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
+        current_time_placeholder.write(f"# Current Time:")
+        current_time_placeholder.write(f"## {current_time}")
+        time.sleep(1)
