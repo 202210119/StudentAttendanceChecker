b505f4f97cb5a2ede4edd327288a8f3dc8cdb9f6
202210119 <202210119@feualabang.edu.ph>
2024-05-10 21:12:19 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,33 +1,29 @@
+import streamlit as st
+
 class Student:
-    @staticmethod
-    def get_student():
-        if "student" not in st.session_state:
-            st.session_state.student = Student()
-        return st.session_state.student
+  @staticmethod
+  def get_student():
+    if "student" not in st.session_state:
+      st.session_state.student = Student()
+    return st.session_state.student
 
-    def __init__(self):
-        if "classes" not in st.session_state:
-            st.session_state.classes = {}  # Dictionary to store class information
+  def __init__(self):
+    if "classes" not in st.session_state:
+      st.session_state.classes = {}
 
-    def join_class(self, class_name):
-        # Check if the class_name exists
-        if class_name in st.session_state.classes:
-            if st.session_state.username not in st.session_state.classes[class_name]:
-                st.session_state.classes[class_name].append(st.session_state.username)
-                return True  # Return True on successful join
-            else:
-                st.warning("You are already in this class.")
-        else:
-            st.session_state.classes[class_name] = [st.session_state.username]
-            return True  # Return True on successful join
+  def join_class(self, class_name):
+    # Check if the class_name exists in the session state
+    if class_name in st.session_state.classes:
+      if st.session_state.username not in st.session_state.classes[class_name]:
+        st.session_state.classes[class_name] = [st.session_state.username]  # Create a list with username
+        return True  # Return True on successful join
+      else:
+        st.warning("You are already in this class.")
+    else:
+      st.session_state.classes[class_name] = [st.session_state.username]  # Create a new class entry with username list
+      return True  # Return True on successful join
 
-        return False  # Return False if joining fails
+    return False  # Return False if joining fails
 
-    def get_student_classes(self):
-        return list(st.session_state.classes.keys())  # Return list of class names
-
-    def get_all_enrolled_students(self):
-        all_students = []
-        for class_name, enrolled_users in st.session_state.classes.items():
-            all_students.extend(enrolled_users)  # Extend the list with all usernames
-        return all_students
+  def get_student_classes(self):
+    return list(st.session_state.classes.keys())
