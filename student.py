ee20b8979783bf150e34f7599a0418009a5ccf9f
202210119 <202210119@feualabang.edu.ph>
2024-05-10 22:19:38 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,3 +1,5 @@
+# student.py
+
 import streamlit as st
 
 class Student:
@@ -12,19 +14,17 @@ class Student:
             st.session_state.classes = {}
 
     def join_class(self, class_name):
-    if class_name in st.session_state.classes:
-        if st.session_state.username not in st.session_state.classes[class_name]:
-            st.session_state.classes[class_name] += [st.session_state.username]
-            return True
+        if class_name in st.session_state.classes:
+            if st.session_state.username not in st.session_state.classes[class_name]:
+                st.session_state.classes[class_name] = [st.session_state.username]
+                return True
+            else:
+                st.warning("You are already in this class.")
         else:
-            st.warning("You are already in this class.")
-    else:
-        st.session_state.classes[class_name] = [st.session_state.username]
-        return True
+            st.session_state.classes[class_name] = [st.session_state.username]
+            return True
 
-    return False
+        return False
 
-def get_available_classes(self):
-    all_classes = st.session_state.classes.keys()
-    joined_classes = self.get_student_classes()
-    return [class_name for class_name in all_classes if class_name not in joined_classes]
+    def get_student_classes(self):
+        return list(st.session_state.classes.keys())
