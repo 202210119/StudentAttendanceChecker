d84c3d883f254c9e0a506326e79e033fc56807f2
202210119 <202210119@feualabang.edu.ph>
2024-05-10 19:06:04 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,38 +1,27 @@
+# student.py
 import streamlit as st
-from teacher import Teacher
 
 class Student:
     @staticmethod
     def get_student(username):
         if "students" not in st.session_state:
             st.session_state.students = {}
-        students = st.session_state.students
-        return students.get(username)
+        if username not in st.session_state.students:
+            st.session_state.students[username] = Student()
+        return st.session_state.students[username]
 
-    def __init__(self, username):
-        self.username = username
+    def __init__(self):
+        if "classes" not in st.session_state:
+            st.session_state.classes = {}
 
-    def join_class(self, class_code):
-        teacher = Teacher.get_teacher()
-        teacher_classes = teacher.get_teacher_classes()
-        if class_code in teacher_classes:
-            if self.username not in teacher_classes[class_code]:
-                teacher_classes[class_code].append(self.username)
-                self._update_student_classes(class_code)
-                return True
-        return False
-
-    def _update_student_classes(self, class_code):
-        if "students" not in st.session_state:
-            st.session_state.students = {}
-        students = st.session_state.students
-        if self.username not in students:
-            students[self.username] = {"classes": {}}
-        student_data = students[self.username]
-        student_data["classes"][class_code] = True
+    def join_class(self, class_name, teacher_classes):
+        if class_name in teacher_classes:
+            if class_name not in st.session_state.classes:
+                st.session_state.classes[class_name] = []
+            st.session_state.classes[class_name].append(self)
+            return True
+        else:
+            return False
 
     def get_student_classes(self):
-        student_data = self.get_student(self.username)
-        if student_data is None:
-            return []
-        return list(student_data.get("classes", {}).keys())
+        return list(st.session_state.classes.keys())
