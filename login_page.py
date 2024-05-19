e79ceb86fe7bc2dfe5de52ee6210c026305968ae
202210119 <202210119@feualabang.edu.ph>
2024-05-19 19:58:45 +0800

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,33 +1,18 @@
 import streamlit as st
 from google_sheets import get_all_records, append_row
-
-SHEET_NAME = "StudentAttendanceChecker"  # Your Google Sheets name
-WORKSHEET_NAME = "Users" 
+from authentication import login, logout
 
 def login_page():
     st.title("Login")
-
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     
     if st.session_state.get("logged_in", False):
         st.info(f"You are logged in as {st.session_state.user_type.capitalize()}")
         if st.button("Logout"):
-            st.session_state["logged_in"] = False
+            logout()
             st.experimental_rerun()
     else:
         if st.button("Login"):
-            users = get_all_records(SHEET_NAME, WORKSHEET_NAME)
-            for user in users:
-                if user["username"] == username and user["password"] == password:
-                    st.session_state["logged_in"] = True
-                    st.session_state["username"] = username
-                    st.session_state["user_type"] = user["user_type"]
-                    st.experimental_rerun()
-            st.error("Invalid username or password")
-
-    if st.button("Register"):
-        st.write("Please use the registration page to sign up.")
-
-if __name__ == "__main__":
-    login_page()
+            if login(username, password):
+                st.experimental_rerun()
